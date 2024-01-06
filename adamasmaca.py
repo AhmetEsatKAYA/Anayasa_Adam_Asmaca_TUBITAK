2a600e0236d862947ea9faa49c975f4b6b2a5292
AhmetEsatKAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2024-01-06 21:28:53 +0300

    Update adamasmaca.py to 105. Version

--- adamasmaca.py
+++ adamasmaca.py
@@ -1,87 +1,72 @@
 import random
-import turtle
 import time
-import tkinter
-adam = turtle.Turtle()
-adam.speed(0) 
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox as kutucuk
 
-def adam_cizme(sayi):
-    if sayi == 5:
-        adam.penup()
-        adam.goto(-50, 120)
-        adam.pendown()
-        adam.circle(50)
-        
-    elif sayi == 4:
-        adam.left(90)
-        adam.penup()
-        adam.forward(50)
-        adam.right(90) 
-        adam.forward(50)
-        adam.pendown()
-        adam.forward(150)
-        
-    elif sayi == 3:
-        adam.left(45)
-        adam.forward(80)
-
-    elif sayi == 2:
-        adam.left(180)
-        adam.penup()
-        adam.forward(80)
-        adam.left(90)
-        adam.pendown()
-        adam.forward(80)
-
-    elif sayi == 1:
-        adam.left(180)
-        adam.forward(80)
-        adam.left(45)
-        adam.forward(100)
-        adam.right(50)
-        adam.forward(80)
-
-    elif sayi == 0:
-        adam.left(180)
-        adam.forward(80)
-        adam.right(80)
-        adam.forward(80)
-        time.sleep(1)
-
+root = tk.Tk()
+harfgir = tk.Entry(root)
+harfgir.pack()
+harfac = tk.Button(root, text="Dene", command=lambda: dene())
+harfac.pack()
+adamalan = tk.Canvas(root, bg="white", height=500, width=500)
+adamalan.pack()
+def dene():
+    dnx(harfgir.get())
+    harfgir.delete(0, tk.END)
 def oyun():
-    adam.penup()
-    adam.goto(0, 250)
-    adam.right(90)
-    adam.pendown()
-    adam.forward(80)
+    adamciz(0)
+    global bilinenler
+    global yanlislar
+    global dogrular
+    global olusankelime
+    global x
+    global can
+    global kelime
     bilinenler = []
+    yanlislar = []
+    dogrular = []
     olusankelime = ""
     x = ""
-    can = 6
+    can = 0
     kelime = random.choice(liste)
     print(len(kelime) * "_")
-    while True:
-            gelenharf = input('Tahminini gir: ')
-            if gelenharf in bilinenler:
-                print("Bunu daha önce yazmıştın")
-            else:
-                bilinenler.append(gelenharf)
-                if gelenharf in kelime:
-                    print("Doğru bildin")
+def dnx(gelenharf):
+            global bilinenler
+            global yanlislar
+            global dogrular
+            global olusankelime
+            global x
+            global can
+            global kelime
+            
+            if len(gelenharf) == 1:
+                if gelenharf in Harfler:
+                    if gelenharf in bilinenler:
+                        print("Bunu daha önce yazmıştın")
+                    else:
+                        bilinenler.append(gelenharf)
+                        if gelenharf in kelime:
+                            print("Doğru bildin")
+                            dogrular.append(gelenharf)
+                        else:
+                            print("Yanlış bildin")
+                            yanlislar.append(gelenharf)
+                            can += 1
+                            adamciz(can)
+                            if can == 6:
+                                print("Oyun bitti")
+                                print("Kaybettin")
+                                print("Doğru cevap " + kelime + " olacaktı")
+                                print("Doğru bildiklerin: "  + ", ".join(dogrular))
+                                print("Yanlış bildiklerin: " + ", ".join(yanlislar))
+                                kararver(False)
+                                oyun()
+                            print(str(6-can) + " canın kaldı")
                 else:
-                    print("Yanlış bildin")
-                    can -= 1
-                    adam_cizme(can)
-                    if can == 0:
-                        print("Oyun bitti")
-                        print("Doğru cevap " + kelime + " olacaktı")
-                        print("Yeni oyun hazırlanıyor..")
-                        adam.clear() 
-                        adam.reset()
-                        adam.speed(0)
-                        oyun()
-                    print(str(can) + " canın kaldı")
-
+                    print("Lütfen harf gir")
+            else:
+                print("Lütfen 1 harf girin")
             olusankelime = ""
             for harf in kelime:
                 if harf in bilinenler:
@@ -93,12 +78,31 @@ def oyun():
             print(olusankelime)
             if olusankelime == kelime:
                 print("Kazandın")
-                print("Yeni oyun hazırlanıyor..")
-                adam.clear() 
-                adam.reset()
-                adam.speed(0)
+                print("Doğru bildiklerin: "  + ", ".join(dogrular))
+                print("Yanlış bildiklerin: " + ", ".join(yanlislar))
+                kararver(True)
                 oyun()
-
+def kararver(kdurum):
+    karar = kutucuk.askquestion('Devam edilsin mi?', 'Devam edilsin mi?')
+    if karar == 'yes':
+        print("Yeni oyun başlatılıyor...")
+    else:
+        exit()
+def adamciz(adamdurum):
+    if adamdurum == 6:
+        print("sol bacak")
+    elif adamdurum == 5:
+        print("sağ bacak")
+    elif adamdurum == 4:
+        print("sol kol")
+    elif adamdurum == 3:
+        print("sağ kol")
+    elif adamdurum == 2:
+        print("gövde")
+    elif adamdurum == 1:
+        print("kafa")
+    elif adamdurum == 0:
+        print("ip ve askı")
 liste = ["hürriyet", "maksat", "yoksun",
         "sansür", "isyan", "makul",
         "esas", "muamele", "beyan",
@@ -111,4 +115,7 @@ liste = ["hürriyet", "maksat", "yoksun",
         "nüfuz", "usul", "cevaz", "rücu",
         "isnat", "müsadere", "müeyyide"]
 
+Harfler = "abcçdefgğhıijklmnoöprsştuüvyz"
+
 oyun()
+root.mainloop()
